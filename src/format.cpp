a1d4220b957789d7ba8206f82423f8c282ce6a83
huanglanzhiguan <huanglanzhiguan@gmail.com>
2024-05-14 16:50:29 +0800

    back up.

--- src/format.cpp
+++ src/format.cpp
@@ -7,7 +7,7 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) {
+string Format::ElapsedTime(long seconds) {
   long hours = seconds / 3600;
   long minutes = (seconds % 3600) / 60;
   long sec = seconds % 60;
